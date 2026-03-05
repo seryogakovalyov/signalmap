3b7800200ab93fe618113597b977a25c3c395b64
Sergey Kovalov <seryogakovalyov@gmail.com>
2026-03-05 16:07:05 +0000

    fix(js): popup closing

--- public/js/map-page.js
+++ public/js/map-page.js
@@ -38,7 +38,7 @@
 
     const map = L.map('report-map', {
         zoomControl: false,
-        closePopupOnClick: false,
+        closePopupOnClick: true,
     });
     const mobileControlsMediaQuery = window.matchMedia('(max-width: 767.98px)');
     let zoomControl = null;
